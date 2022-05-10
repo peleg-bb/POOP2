d66ef5ad9ffccad9b3479c9582d497dcb2d34743
Lizet <lizetpo@post.bgu.ac.il>
2022-05-10 11:41:49 +0300

    add

--- main.cpp
+++ main.cpp
@@ -3,15 +3,30 @@
 #include "Person.h"
 #include "Wholesaler.h"
 #include "Florist.h"
+#include "Delivery_Person.h"
 
 
 int main(int argc, char** argv)
 {
 	std::cout << "Hello " << std::endl;
-	Person* chris = new Person("Chris");
+	
 	Person* robin = new Person("Robin");
-	Wholesaler* john = new Wholesaler("John");
-	Florist* jake = new Florist("Jake", john);
+	Flower_Arranger* flora = new Flower_Arranger("Flora");
+	Delivery_Person* dylan = new Delivery_Person("Dylan");
+	Gardner* garret = new Gardner("Garret");
+	Grower* gray = new Grower("Gray", garret);
+	Wholesaler* watson = new Wholesaler("Watson", gray);
+	Florist* fred = new Florist("Fred", watson, flora, dylan);
+	Person* chris = new Person("Chris");
 	std::vector<std::string> flowers = { "Buttercup", "Primrose", "Hibiscus" };
-	robin->orderFlowers(jake, chris, flowers);
+	chris->orderFlowers(fred, robin, flowers);
+
+	delete robin;
+	delete chris;
+	delete flora;
+	delete dylan;
+	delete garret;
+	delete gray;
+	delete watson;
+	delete fred;
 }
